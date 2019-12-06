7afebe405087a07d0e321f01d04d11bd0669c36b
Rob Lemley <rob@thunderbird.net>
2019-12-06 17:19:39 -0500

    QOwnNotes commit

--- scripts/unified_check.sh
+++ scripts/unified_check.sh
@@ -8,8 +8,8 @@ rm -rf /tmp/browser /tmp/mail /tmp/browser_unified /tmp/mail_unified
 mkdir /tmp/browser_unified /tmp/mail_unified
 
 
-MOZCONFIG=mozconfig.empty MOZ_OBJDIR=/tmp/browser mach configure --enable-application=browser
-MOZCONFIG=mozconfig.empty MOZ_OBJDIR=/tmp/mail mach configure --enable-application=comm/mail
+MOZCONFIG=mozconfig.empty MOZ_OBJDIR=/tmp/browser ./mach configure --enable-application=browser
+MOZCONFIG=mozconfig.empty MOZ_OBJDIR=/tmp/mail ./mach configure --enable-application=comm/mail
 
 find /tmp/browser -name \*Unified\* -exec cp '{}' /tmp/browser_unified/ \;
 find /tmp/mail -name \*Unified\* -exec cp '{}' /tmp/mail_unified/ \;
