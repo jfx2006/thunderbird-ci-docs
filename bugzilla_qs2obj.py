ba40e2685bbd419cd705ce72492189a6e43bda8b
Rob Lemley <rob@thunderbird.net>
2021-08-04 11:29:33 -0400

    Make the output of bugzilla_qs2obj.py more useful.

--- bugzilla_qs2obj.py
+++ bugzilla_qs2obj.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python
 
 import sys
-from urllib.parse import (
-    urlparse,
-    parse_qs
-)
+from urllib.parse import urlparse, parse_qs
 import json
 
 urlstring = sys.argv[1]
 
 url = urlparse(urlstring)
 qp = parse_qs(url.query)
-qp['include_fields'] = ''
+qp["include_fields"] = ""
 
 try:
-    del qp['columnlist']
-    del qp['known_name']
+    del qp["columnlist"]
+    del qp["known_name"]
 except KeyError:
     pass
+
+query_dict = {
+    "description": "",
+    "fetch_cols": [
+        "id",
+        "summary",
+        "status",
+        "product",
+        "component",
+        "target_milestone",
+        "priority",
+        "bug_severity",
+        "cf_last_resolved",
+        "cf_status_thunderbird_%CHANNEL%",
+    ],
+    "queryparams": {},
+}
 
 print(qp)
 for key, value in qp.items():
@@ -25,4 +39,6 @@ for key, value in qp.items():
         if len(value) == 1:
             qp[key] = value[0]
 
-print(json.dumps(qp, indent=2))
+query_dict["queryparams"] = qp
+
+print(json.dumps(query_dict, indent=2))
