ae9f64c311e4a8e4ba68356645918354a6fa8d85
Rob Lemley <rob@thunderbird.net>
2022-02-26 21:46:46 -0500

    Add future release dates to landing

--- release-docs/assets/js/landing.js
+++ release-docs/assets/js/landing.js
@@ -4,25 +4,90 @@
 
 import { ThunderbirdProductDetails } from './product-details.js'
 
+
 function Landing() {
+    const calId = "jgarvd0cmlt3599e7gthhk01a8@group.calendar.google.com"
+    const calKey = "AIzaSyAltD4ljzWSekk8ZAh5FCDqC5gvMz8Qkwg"
+    const RELEASE_URL = `https://www.googleapis.com/calendar/v3/calendars/${calId}/events?key=${calKey}&orderBy=startTime&singleEvents=true&timeZone=UTC`
+
     function capitalizeFirstLetter(val) {
         return val.charAt(0).toUpperCase() + val.slice(1)
+    }
+
+    function getDateRange() {
+        let start = new Date()
+        let start_str = start.toISOString()
+        start.setMonth(start.getMonth() + 1)
+        let end_str = start.toISOString()
+        return {timeMin: start_str, timeMax: end_str}
     }
 
     function updateUI(released_versions) {
         for (let [channel, version] of Object.entries(released_versions)) {
             const div = document.getElementById(channel)
             let inner = new DocumentFragment()
-            let h2 = document.createElement("h2")
-            h2.innerHTML = capitalizeFirstLetter(channel)
+            let span = document.createElement("span")
+            span.classList.add("release")
+            span.textContent = capitalizeFirstLetter(channel)
 
             let a = document.createElement("a")
             a.href = `dashboard/#pollbot/thunderbird/${channel}`
-            a.innerHTML = version.toString()
+            a.textContent = version.toString()
 
-            inner.appendChild(h2)
+            inner.appendChild(span)
             inner.appendChild(a)
             div.appendChild(inner)
+        }
+    }
+
+    function buildEventURL() {
+        const range = getDateRange()
+        let api_url = new URL(RELEASE_URL)
+        api_url.searchParams.append("timeMin", range.timeMin)
+        api_url.searchParams.append("timeMax", range.timeMax)
+        return api_url
+    }
+
+    function buildUpcoming(eventData) {
+        let upcomingReleases = {}
+        let event
+        for (event of eventData.items) {
+            if (event.summary === "Thunderbird Status Meeting") {
+                continue
+            } else if (event.summary.startsWith("Thunderbird ")) {
+                if (event.start.date !== undefined) {
+                  upcomingReleases[event.summary] = event.start.date
+                }
+            }
+        }
+        return upcomingReleases
+    }
+
+    function setUpcoming(releases) {
+        /* <table id="upcoming-body">
+          <tr class="upcoming-release">
+            <td class="upcoming-version">98.0b3</td>
+            <td class="upcoming-date">February 28, 2022</td>
+          </tr>
+        </table> */
+        let table = document.getElementById("upcoming-body")
+        const fmt_options = {year: 'numeric', month: 'long', day: 'numeric'}
+        for (let [desc, date] of Object.entries(releases)) {
+            let row = document.createElement("tr")
+            row.classList.add("upcoming-release")
+            let version = document.createElement("td")
+            version.classList.add("upcoming-version")
+            version.textContent = desc
+            let rel_date = Intl.DateTimeFormat(
+              navigator.language,
+              fmt_options
+            ).format(new Date(date))
+            let date_elem = document.createElement("td")
+            date_elem.classList.add("upcoming-date")
+            date_elem.textContent = rel_date
+            row.appendChild(version)
+            row.appendChild(date_elem)
+            table.appendChild(row)
         }
     }
 
@@ -34,6 +99,16 @@ function Landing() {
         })
         .catch((err) => {
             alert(err)
+        })
+
+    let api_url = buildEventURL()
+    fetch(api_url.href)
+        .then(async function(result)  {
+            let upcomingReleases = buildUpcoming(await result.json())
+            setUpcoming(upcomingReleases)
+        })
+        .catch((err) => {
+            throw(err)
         })
 }
 
